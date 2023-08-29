dcb9b26f629daef4c5d2aa2f18dcb36eaafb4721
Zucci Daniel <zuccizucci555@gmail.com>
2023-08-29 14:56:44 +0100

    pushing to main

--- src/screens/dashboard/styles.ts
+++ src/screens/dashboard/styles.ts
@@ -23,7 +23,7 @@ export const DashboardScreenStyles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 50,
     backgroundColor: pallete.screen,
-    height: screenHeight - 50,
+    height: screenHeight - 150,
   },
   sheetWrapper: {
     flex: 1,
@@ -37,7 +37,6 @@ export const DashboardScreenStyles = StyleSheet.create({
   time: {
     height: 50,
     width: '100%',
-    backgroundColor: 'red',
   },
   emptyContainer: {
     flex: 1,
