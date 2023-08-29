b0f2acf5cd9092982ed0a606731632ffd1dd4019
Zucci Daniel <zuccizucci555@gmail.com>
2023-08-29 14:30:38 +0100

    updates

--- src/screens/dashboard/useDashboard.ts
+++ src/screens/dashboard/useDashboard.ts
@@ -11,9 +11,11 @@ import {inputType, MedicationReminder, MedicationTime} from './type';
 import {setCurrentUser} from '../../redux/global-store/storeSlice';
 import {logThis} from '../../helpers';
 import {generateUniqueId} from '../../helpers/general';
+import {useSilentToast} from '../../presenters/h-toast';
 
 export const useDashboard = () => {
   const dispatch: any = useDispatch();
+  const showToast = useSilentToast();
   const addMedSheetRef = useRef(null);
   //accessing the user state from redux
   const {user} = useSelector((state: storeSliceType) => state.storeReducer);
@@ -225,11 +227,15 @@ export const useDashboard = () => {
           scheduledTime,
           frequency,
         );
-
         noticeIDs.push({id, day, time: scheduledTime});
       }
 
-      return noticeIDs;
+      if (noticeIDs.length > 1) {
+        return noticeIDs;
+      } else {
+        showToast('info', "Couldn't schedule your notification! try again");
+        return noticeIDs;
+      }
     } catch (error) {
       logThis(error);
       return [];
